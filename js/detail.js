32d6f325fbd236367668b4e067ca52a487e99afd
yanuardhijefry-GRS <yanuardhijefry@gmail.com>
2026-07-17 18:59:56 +0700

    Add files via upload

---
+++ js/detail.js
@@ -0,0 +1,31 @@
+if(localStorage.getItem("login")!="true"){
+location="index.html";
+}
+
+let anggota=
+JSON.parse(localStorage.getItem("anggota"))||[];
+
+let index=
+localStorage.getItem("detailIndex");
+
+if(index==null){
+
+location="data-anggota.html";
+
+}
+
+let a=anggota[index];
+
+document.getElementById("foto").src=a.foto;
+
+document.getElementById("nama").innerHTML=a.nama;
+
+document.getElementById("id").innerHTML=a.id;
+
+document.getElementById("jabatan").innerHTML=a.jabatan;
+
+document.getElementById("alamat").innerHTML=a.alamat;
+
+document.getElementById("hp").innerHTML=a.hp;
+
+document.getElementById("tanggal").innerHTML=a.tanggal;
