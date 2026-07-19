15f14ad0a8eb97cfcc1eb233e234ce5d01c54648
yanuardhijefry-GRS <yanuardhijefry@gmail.com>
2026-07-19 07:07:25 +0700

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,3 +4,12 @@ const sidebar = document.querySelector(".sidebar");
 menuBtn.onclick = function(){
     sidebar.classList.toggle("active");
 };
+const menu=document.getElementById("menuBtn");
+
+const sidebar=document.getElementById("sidebar");
+
+menu.onclick=function(){
+
+sidebar.classList.toggle("active");
+
+}
